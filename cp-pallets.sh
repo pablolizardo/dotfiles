1d82a648bf218b03fc74531b45158957a970c1a1
pablolizardo <pablo_lizardo@yahoo.com.ar>
2014-08-22 20:44:27 -0300

    little changes

--- cp-pallets.sh
+++ cp-pallets.sh
@@ -1,4 +1,4 @@
-echo "linkeando paletas a inkscape y a gimp"
+echo "linkeando paletas a Inkscape y a Gimp"
 ln ./pallets/*.* ~/.config/inkscape/palettes/
 ln ./pallets/*.* ~/.gimp-2.8/palettes/
 echo "terminado"
